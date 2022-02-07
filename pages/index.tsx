96c525ac124a1848f0ff9605137e9f2f5c1b6f63
DedeSnrwn13 <sunarwandede13@gmail.com>
2022-02-07 14:42:42 +0700

    import aos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import AOS from 'aos';
 import Navbar from './../components/organisms/Navbar/Index';
 import MainBanner from '../components/organisms/MainBanner/Index';
 import TransactionStep from './../components/organisms/TransactionStep/Index';
@@ -7,6 +6,7 @@ import FeatureGame from './../components/organisms/FeatureGame/Index';
 import Reached from './../components/organisms/Reached/Index';
 import Story from './../components/organisms/Story/Index';
 import Footer from './../components/organisms/Footer/Index';
+import AOS from 'aos';
 
 export default function Home() {
   useEffect(() => {
